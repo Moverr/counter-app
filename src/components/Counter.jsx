32f50ea6caebfb43338d802d1e7bdfb270719128
Muyinda Rogers <moverr@gmail.com>
2018-09-19 08:43:05 +0300

    Adding Class Attributes

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -4,20 +4,20 @@ class Counter extends Component {
     //State is a special property, it includes data that the component needs
     state = {
         count:0,
-        imageUrl:'https://picsum.photos/100'
+        // imageUrl:'https://picsum.photos/100'
     };
     render() { 
         return(
             <React.Fragment>
-                <img src={this.state.imageUrl} alt= "" />
-                <span>{this.formatCount()}</span>
+                {/* <img src={this.state.imageUrl} alt= "" /> */}
+                <span className="badge badge-primary m-2">{this.formatCount()}</span>
                 <button >Increment</button>
             </React.Fragment>
         ); 
     }
     formatCount(){
         const {count} = this.state;
-        return count === 0 ? <h1>ZERO</h1> : count ;
+        return count === 0 ? "ZERO" : count ;
     }
 }
  
