60beaa97ca077cac8c2541e99249e4b7f1e97e73
Muyinda Rogers <moverr@gmail.com>
2018-09-19 08:57:01 +0300

    Dynamic Classes

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -3,7 +3,7 @@ import React,{Component} from 'react';
 class Counter extends Component {
     //State is a special property, it includes data that the component needs
     state = {
-        count:1,
+        count:0,
         // imageUrl:'https://picsum.photos/100'
     };
     // styles = {
@@ -11,17 +11,23 @@ class Counter extends Component {
     //     fontWeight:'bold'
     // }
     render() { 
-        let spnClasses = "badge  m-2";
-        spnClasses += (this.state.count === 0 ) ? " badge-warning " :" badge-primary  " ;
+    
+
         return(
             <React.Fragment>
                 {/* <img src={this.state.imageUrl} alt= "" /> */}
                 {/* <span style={this.styles} className="badge badge-primary m-2">{this.formatCount()}</span> */}
-                <span className={spnClasses}>{this.formatCount()}</span>
+                <span className={ this.getBadgeClasses()}>{this.formatCount()}</span>
                 <button  className="btn btn-secondary btn-sm" >Increment</button>
             </React.Fragment>
         ); 
     }
+    getBadgeClasses() {
+        let spnClasses = "badge  m-2";
+        spnClasses += (this.state.count === 0) ? " badge-warning " : " badge-primary  ";
+        return spnClasses;
+    }
+
     formatCount(){
         const {count} = this.state;
         return count === 0 ? "ZERO" : count ;
