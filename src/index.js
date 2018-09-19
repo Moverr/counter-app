90532f654c549ae459c4f7be9ce5f2b6fd44cda2
Muyinda Rogers <moverr@gmail.com>
2018-09-19 08:06:19 +0300

    Counter.jsx

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Counter from './components/Counter'
 import registerServiceWorker from './registerServiceWorker';
+import 'bootstrap/dist/css/bootstrap.css'
+ 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Counter />, document.getElementById('root'));
 registerServiceWorker();
+ 
