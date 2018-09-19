6722fb483d7c572537f745a9327ac2d3e9dc9fe1
Muyinda Rogers <moverr@gmail.com>
2018-09-19 08:40:08 +0300

    Adding Image

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,13 +1,23 @@
 import React,{Component} from 'react';
 
 class Counter extends Component {
-    
+    //State is a special property, it includes data that the component needs
+    state = {
+        count:0,
+        imageUrl:'https://picsum.photos/100'
+    };
     render() { 
         return(
-            <React.Fragment><h1>Hello Mover</h1>
-                <button >Greate Is the Lord</button>
+            <React.Fragment>
+                <img src={this.state.imageUrl} alt= "" />
+                <span>{this.formatCount()}</span>
+                <button >Increment</button>
             </React.Fragment>
-        );
+        ); 
+    }
+    formatCount(){
+        const {count} = this.state;
+        return count === 0 ? <h1>ZERO</h1> : count ;
     }
 }
  
